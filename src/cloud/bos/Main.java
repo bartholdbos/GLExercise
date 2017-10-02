0018e31c02ec115289d5f9ee639607e4e3038ce2
Barthold Bos <bartholdbos@hotmail.com>
2017-10-03 00:55:07 +0200

    Add ShaderProgram and Working VBO's

--- src/cloud/bos/Main.java
+++ src/cloud/bos/Main.java
@@ -5,25 +5,23 @@ import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.system.MemoryStack;
 
-import java.nio.FloatBuffer;
+import java.io.File;
+import java.io.IOException;
 import java.nio.IntBuffer;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
-import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
-import static org.lwjgl.system.MemoryUtil.memAllocFloat;
-import static org.lwjgl.system.MemoryUtil.memFree;
 
 public class Main {
 
     private long window;
-
-    private State state = new State();
 
     public Main() {
         init();
@@ -38,20 +36,21 @@ public class Main {
             throw new IllegalStateException("Unable to initialize GLFW");
 
         glfwDefaultWindowHints();
-//        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-        window = glfwCreateWindow(400, 400, "GLExercise", NULL, NULL);
+        window = glfwCreateWindow(800, 600, "GLExercise", NULL, NULL);
         if (window == NULL)
             throw new RuntimeException("Failed to create GLFW window");
 
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-                glfwSetWindowShouldClose(window, true);
+        glfwMakeContextCurrent(window);
+
+        glfwSetFramebufferSizeCallback(window, (window, width, height) -> {
+            glViewport(0, 0, width, height);
         });
 
         try (MemoryStack stack = stackPush()) {
@@ -69,21 +68,59 @@ public class Main {
             );
         }
 
-        glfwMakeContextCurrent(window);
+
         glfwSwapInterval(1);
         glfwShowWindow(window);
+        GL.createCapabilities();
     }
 
     private void loop() {
-        GL.createCapabilities();
-        state.init();
+        ShaderProgram shaderProgram = new ShaderProgram();
+        try {
+            shaderProgram.loadShader(new File("shaders/vertex.shader"), GL_VERTEX_SHADER);
+            shaderProgram.loadShader(new File("shaders/fragment.shader"), GL_FRAGMENT_SHADER);
+            shaderProgram.link();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        float vertices[] ={
+                0.5f,  0.5f, 0.0f,   // top right
+                0.5f, -0.5f, 0.0f,   // bottom right
+                -0.5f, -0.5f, 0.0f,  // bottom left
+                -0.5f,  0.5f, 0.0f   // top left
+        };
 
+        int indices[] = { // new
+                0, 1, 3,
+                1, 2, 3
+        };
+
+        int VAO = glGenVertexArrays();
+        int VBO = glGenBuffers();
+        int EBO = glGenBuffers(); // new
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         while (!glfwWindowShouldClose(window)) {
+            processInput(window);
+
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            state.draw();
+            shaderProgram.bind();
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
             glfwSwapBuffers(window);
             glfwPollEvents();
@@ -96,6 +133,12 @@ public class Main {
 
         glfwTerminate();
         glfwSetErrorCallback(null).free();
+    }
+
+    private void processInput(long window){
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+            glfwSetWindowShouldClose(window, true);
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/cloud/bos/ShaderProgram.java
@@ -0,0 +1,61 @@
+package cloud.bos;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.lwjgl.opengl.GL11.GL_FALSE;
+import static org.lwjgl.opengl.GL11.GL_INVALID_ENUM;
+import static org.lwjgl.opengl.GL20.*;
+
+public class ShaderProgram {
+
+    private final int programId;
+
+    public ShaderProgram() {
+        programId = glCreateProgram();
+
+        if (programId == 0) {
+            throw new RuntimeException("Could not create ShaderProgram");
+        }
+    }
+
+    public void loadShader(File shaderFile, int shaderType) throws IOException {
+        int shaderId = glCreateShader(shaderType);
+
+        if (shaderId == 0) {
+            throw new RuntimeException("Could not create Shader");
+        } else if (shaderId == GL_INVALID_ENUM) {
+            throw new RuntimeException("Invalid ShaderType: " + shaderType);
+        }
+
+        glShaderSource(shaderId, readFileToString(shaderFile));
+        glCompileShader(shaderId);
+
+        if (glGetShaderi(shaderId, GL_COMPILE_STATUS) == GL_FALSE) {
+            throw new RuntimeException("Error compiling Shader code: " + glGetShaderInfoLog(shaderId));
+        }
+
+        glAttachShader(programId, shaderId);
+    }
+
+    private String readFileToString(File file) throws IOException {
+        return new String(Files.readAllBytes(file.toPath()));
+    }
+
+    public void link() {
+        glLinkProgram(programId);
+
+        if (glGetProgrami(programId, GL_LINK_STATUS) == GL_FALSE) {
+            throw new RuntimeException("Error linking ShaderProgram: " + glGetProgramInfoLog(programId));
+        }
+    }
+
+    public void bind() {
+        glUseProgram(programId);
+    }
+
+    public static void unbind() {
+        glUseProgram(0);
+    }
+}
